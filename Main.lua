711d72dcbc0e91ce675562079a618f2b6e177590
Eskymaq <wewerka881@gmail.com>
2026-03-12 16:08:07 +0100

    Enhance options and add Speed/Fly section
    Added Fly and FlySpeed options, created new Speed/Fly section.

--- Main.lua
+++ Main.lua
@@ -1,22 +1,21 @@
 -- Main.lua
 local REPO_URL = "https://raw.githubusercontent.com/Eskymaq/quantum/main/"
-
-local Options = {MonsterESP = false, PlayerESP = false, Noclip = false, WalkSpeed = false, WalkSpeedValue = 16}
+local Options = {MonsterESP = false, PlayerESP = false, Noclip = false, WalkSpeed = false, WalkSpeedValue = 16, Fly = false, FlySpeed = 60}
 
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/nfpw/XXSCRIPT/refs/heads/main/Library/Module.lua"))()
 local window = library:CreateWindow({WindowName="Quantum Evolution"}, game.CoreGui)
 
--- Vytvoření tabů a sekcí
 local main_tab = window:CreateTab("Main")
 local esp_tab = window:CreateTab("ESP")
 
 local main_sec = main_tab:CreateSection("Movement")
+local walk_sec = main_tab:CreateSection("Speed/Fly")
 local esp_sec = esp_tab:CreateSection("Visuals")
 
 -- Načtení modulů
 local movementMod = loadstring(game:HttpGet(REPO_URL .. "Modules/Movement.lua"))()
 local visualsMod = loadstring(game:HttpGet(REPO_URL .. "Modules/Visuals.lua"))()
 
--- Předání sekcí modulům
-if movementMod then movementMod(Options, main_sec) end
+-- Propojení
+if movementMod then movementMod(Options, main_sec, walk_sec) end
 if visualsMod then visualsMod(Options, esp_sec) end
