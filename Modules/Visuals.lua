32ecebd3c36ceb7a4531754fcb1f64927477b302
Eskymaq <wewerka881@gmail.com>
2026-03-12 16:06:17 +0100

    Enhance Monster ESP toggle with logging
    Added print statement to log changes to Monster ESP toggle.

--- Modules/Visuals.lua
+++ Modules/Visuals.lua
@@ -1,15 +1,7 @@
--- Visuals.lua
+-- Modules/Visuals.lua
 return function(Options, esp_sec)
     esp_sec:CreateToggle("Monster ESP", Options.MonsterESP, function(v)
         Options.MonsterESP = v
-    end)
-
-    esp_sec:CreateToggle("Player ESP", Options.PlayerESP, function(v)
-        Options.PlayerESP = v
-    end)
-    
-    -- ESP Loop
-    game:GetService("RunService").RenderStepped:Connect(function()
-        -- Zde by byla logika pro vykreslování
+        print("Monster ESP změněno na:", v)
     end)
 end
