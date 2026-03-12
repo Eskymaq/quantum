bf644732be504ac188ce29ca83f85558fa5fb32e
Eskymaq <wewerka881@gmail.com>
2026-03-12 16:19:07 +0100

    Delete init.lua

--- init.lua
+++
@@ -1,5 +0,0 @@
--- init.lua
-if not getgenv().QuantumLoaded then
-    getgenv().QuantumLoaded = true
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Eskymaq/quantum/main/Main.lua"))()
-end
