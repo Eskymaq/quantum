e782ac691b064eb13c55e5d0af891d90dc39b2c2
Eskymaq <wewerka881@gmail.com>
2026-03-12 16:06:05 +0100

    Refactor Movement module to simplify options
    Removed walk speed and infinite jump functionalities.

--- Modules/Movement.lua
+++ Modules/Movement.lua
@@ -1,8 +1,7 @@
--- Movement.lua
-return function(Options, main_sec, walk_sec, jump_sec)
-    local Players = game:GetService("Players")
-    local LocalPlayer = Players.LocalPlayer
-
+-- Modules/Movement.lua
+return function(Options, main_sec)
+    local LocalPlayer = game:GetService("Players").LocalPlayer
+    
     main_sec:CreateToggle("Noclip", Options.Noclip, function(v)
         Options.Noclip = v
     end)
@@ -12,21 +11,6 @@ return function(Options, main_sec, walk_sec, jump_sec)
             for _, p in pairs(LocalPlayer.Character:GetDescendants()) do
                 if p:IsA("BasePart") then p.CanCollide = false end
             end
-        end
-    end)
-
-    walk_sec:CreateToggle("Enable WalkSpeed", Options.WalkSpeed, function(v)
-        Options.WalkSpeed = v
-        if v then LocalPlayer.Character.Humanoid.WalkSpeed = Options.WalkSpeedValue end
-    end)
-    
-    jump_sec:CreateToggle("Infinite Jump", Options.InfiniteJump, function(v)
-        Options.InfiniteJump = v
-    end)
-
-    game:GetService("UserInputService").JumpRequest:Connect(function()
-        if Options.InfiniteJump then
-            LocalPlayer.Character:FindFirstChildOfClass("Humanoid"):ChangeState("Jumping")
         end
     end)
 end
