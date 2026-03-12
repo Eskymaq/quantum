f266d4e4c5f162f29fe9b93928f28c1c4ed8e71e
Eskymaq <wewerka881@gmail.com>
2026-03-12 16:00:39 +0100

    Refactor Main.lua for improved structure and clarity

--- Main.lua
+++ Main.lua
@@ -1,50 +1,27 @@
 -- Main.lua
 local REPO_URL = "https://raw.githubusercontent.com/Eskymaq/quantum/main/"
 
--- Definice Options
 local Options = {
-    MonsterESP = false, PlayerESP = false,
-    Noclip = false,
-    WalkSpeed = false, WalkSpeedValue = 16,
-    InfiniteJump = false,
-    JumpPowerEnabled = false, JumpPower = 50,
-    Fly = false, FlySpeed = 60,
-    ToggleKeyName = "Insert"
+    MonsterESP = false, PlayerESP = false, Noclip = false,
+    WalkSpeed = false, WalkSpeedValue = 16, InfiniteJump = false
 }
 
--- Inicializace UI
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/nfpw/XXSCRIPT/refs/heads/main/Library/Module.lua"))()
-local window = library:CreateWindow({WindowName="Peak Evolution | V3.3"}, gethui())
+local window = library:CreateWindow({WindowName="Quantum | V3.3"}, gethui())
 
--- Vytvoření tabů a sekcí
+-- Tady vytvoříme sekce
 local main_tab = window:CreateTab("Main")
 local esp_tab = window:CreateTab("ESP")
-local config_tab = window:CreateTab("Settings")
 
 local main_sec = main_tab:CreateSection("Main")
 local walk_sec = main_tab:CreateSection("Walk")
 local jump_sec = main_tab:CreateSection("Jump")
-local fly_sec = main_tab:CreateSection("Fly")
-local esp_sec = esp_tab:CreateSection("Visual")
-local config_sec = config_tab:CreateSection("Settings")
+local esp_sec = esp_tab:CreateSection("Visuals")
 
--- Načtení modulů
-local movementFunc = loadstring(game:HttpGet(REPO_URL .. "Modules/Movement.lua"))()
-local visualsFunc = loadstring(game:HttpGet(REPO_URL .. "Modules/Visuals.lua"))()
+-- Načtení a okamžité spuštění modulů
+local movementMod = loadstring(game:HttpGet(REPO_URL .. "Modules/Movement.lua"))()
+local visualsMod = loadstring(game:HttpGet(REPO_URL .. "Modules/Visuals.lua"))()
 
--- Propojení (posíláme Options a sekce do modulů)
-movementFunc(Options, main_sec, walk_sec, jump_sec, fly_sec)
-visualsFunc(Options, esp_sec)
-
--- Globální věci v Mainu (Config/Unload)
-config_sec:CreateButton("Unload Script", function()
-    library:Destroy()
-end)
-
--- Keybind pro menu
-game:GetService("UserInputService").InputBegan:Connect(function(input, gp)
-    if not gp and input.KeyCode.Name == Options.ToggleKeyName then
-        local my_gui = gethui():GetChildren()[#gethui():GetChildren()]
-        if my_gui then my_gui.Enabled = not my_gui.Enabled end
-    end
-end)
+-- Propojení sekcí s moduly
+movementMod(Options, main_sec, walk_sec, jump_sec)
+visualsMod(Options, esp_sec)
